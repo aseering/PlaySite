0636db3d142177e9575ea156230956a8f40bfeb9
Adam Seering <aseering@gmail.com>
2012-10-28 12:21:55 -0400

    Rework inline_wiki as a standalone/includeable template.  Learn about implicit param's.  Fix some wiki stuff.  Still not editable.

--- app/views/Application.scala
+++ app/views/Application.scala
@@ -3,12 +3,10 @@ package views
 import play.api._
 import play.api.mvc._
 
-import templates._
-
 object Application extends Controller {
-  
-  def index = Action {
-    Ok(template.html.index("Your new application is ready."))
-  }
- 
+
+	def index = Action { implicit request =>
+		Ok(template.html.index("Your new application is ready."))
+	}
+
 }
